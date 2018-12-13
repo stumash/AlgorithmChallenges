0b2ed2cd18c35ed2ed8aca20004ed43b38a01041
stumash <stuart.mashaal@gmail.com>
2018-12-13 03:51:20 -0500

    add interactivity to migratory birds python3

--- hackerrank/migratory_birds/sol.py
+++ hackerrank/migratory_birds/sol.py
@@ -7,3 +7,6 @@ def sol(arr: List[int]) -> int:
         d[a] += 1
     most_viewings = max(d.values())
     return min(a for a in d if d[a] == most_viewings)
+
+input()
+print(sol(list(map(int, input().split(' '))))
