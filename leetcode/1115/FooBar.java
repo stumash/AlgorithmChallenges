5ae46a0808767bc7433106eeb98c9decda9aa202
stumash <stuart.mashaal@gmail.com>
2019-07-25 13:39:16 -0400

    add leetcode/1115 concurrency/foobar

---
+++ leetcode/1115/FooBar.java
@@ -0,0 +1,36 @@
+import java.util.concurrent.Semaphore;
+
+class FooBar {
+    private int n;
+    private Semaphore fooSem, barSem;
+
+    public FooBar(int n) {
+        this.n = n;
+        fooSem = new Semaphore(1);
+        barSem = new Semaphore(0);
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+            fooSem.acquire();
+
+            // printFoo.run() outputs "foo". Do not change or remove this line.
+            printFoo.run();
+
+            barSem.release();
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+            barSem.acquire();
+
+            // printBar.run() outputs "bar". Do not change or remove this line.
+            printBar.run();
+
+            fooSem.release();
+        }
+    }
+}
