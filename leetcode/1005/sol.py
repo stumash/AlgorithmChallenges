d83f3ea7dab5fd42e7fe1bb0bc081a230a8b5af5
Stuart Mashaal <smashaal@hopper.com>
2021-04-18 19:42:33 -0400

    clenaup leetcode 1005

--- leetcode/1005/sol.py
+++ leetcode/1005/sol.py
@@ -6,39 +6,43 @@ class Solution:
         assert(len(A) != 0)
         assert(K >= 0)
 
+        if len(A) == 0:
+            return 0
+
         A = sorted(A)
 
+        total = 0
         negs = 0
-        stop_negs = False
         abs_min = max(abs(A[0]), A[-1])
 
         for a in A:
-            if a >= 0 or negs == K: # reached the end of negative numbers flipped to positive
-                stop_negs = True
-            if not stop_negs:
-                negs += 1
             abs_a = abs(a)
+
             if abs_a < abs_min:
                 abs_min = abs_a
 
-        res = -sum(A[:negs]) # sum the flipped negatives
-        res += sum(A[negs:]) # and the non-flipped everything else
+            # add all numbers to total, flipping at most the first K negative numbers
+            if a < 0 and negs < K:
+                total += abs_a
+                negs += 1
+            else:
+                total += a
 
-        rem = K - negs
-        if rem > 0 and rem % 2 == 1: # if odd number of flips remains
-            res -= 2 * abs_min # smallest available positive number flips to negative
+        # if odd number of flips remains, flip the smallest number counted as positive 
+        if (K - negs) % 2 == 1:
+            total -= (abs_min + abs_min)
 
-        return res
+        return total
 
+
+# test
 sol = Solution()
-
-tests = [
+for (A, K, expected) in [
     (sorted([4, 3, 2]), 1, 5),
     (sorted([3,-1,0,2]), 3, 6),
     (sorted([2,-3,-1,5,-4]), 2, 13),
     (sorted([-8,3,-5,-3,-5,-2]), 6, 22)
-]
-for (A, K, expected) in tests:
+]:
     actual = sol.largestSumAfterKNegations(A, K)
     if actual != expected:
         print('Fail:')
