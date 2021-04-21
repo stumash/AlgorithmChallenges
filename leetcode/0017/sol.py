b5827604e13fd12085aef90f7b642da14b0b5485
Stuart Mashaal <smashaal@hopper.com>
2021-04-21 11:14:58 -0400

    clean up leetcode 17

--- leetcode/0017/sol.py
+++ leetcode/0017/sol.py
@@ -1,22 +1,7 @@
 from typing import List
 
-def mult(self, ss1: List[str], ss2: List[str]) -> List[str]:
-    return [
-        s1 + s2
-        for s1 in ss1
-        for s2 in ss2
-    ]
 
-def reduce(xs, f):
-    result = None
-    for x in xs:
-        if result is None:
-            result = x
-        else:
-            result = f(result, x)
-    return result if result is not Non
-
-self.m = {
+m = {
     '2': ['a', 'b', 'c'],
     '3': ['d', 'e', 'f'],
     '4': ['g', 'h', 'i'],
@@ -27,15 +12,20 @@ self.m = {
     '9': ['w', 'x', 'y', 'z'],
 }
 
+
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         product = None
         for d in digits:
-            assert(d in self.m.keys())
+            assert(d in m.keys())
             if product is None:
-                product = self.m[d]
+                product = m[d]
             else:
-                product = mult(product, self.m[d])
+                product = [
+                    s1 + s2
+                    for s1 in product
+                    for s2 in m[d]
+                ]
         return [] if product is None else product
 
 
