53730cf323bc9ace4d7ce3892e27c260b5e2964b
Stuart Mashaal <stuart.mashaal@gmail.com>
2022-08-15 11:55:32 -0400

    clean up leetcode 0049

--- leetcode/0049/sol.py
+++ leetcode/0049/sol.py
@@ -1,12 +1,17 @@
 from collections import Counter, defaultdict
 from typing import Dict, List
-import json
 
 class Solution:
+    def counter_to_key(self, c: Counter[str]) -> str:
+        result = ''
+        for ch in map(chr, range(ord('a'), ord('z')+1)):
+            result += f'{c[ch]}#'
+        return result
+
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         freq_count_to_word: Dict[str, List[str]] = defaultdict(list)
         for s in strs:
-            key = json.dumps(sorted(Counter(s).items()))
+            key = self.counter_to_key(Counter(s))
             freq_count_to_word[key].append(s)
         return list(freq_count_to_word.values())
 
